2cc16a935a20232e3c932f140ce2dde9ff77c815
Rooh2os <reuben.e.h2os@gmail.com>
2025-07-04 12:24:17 -0500

    Refactor: Improve user experience and error handling for to-do list

--- To Do.py
+++ To Do.py
@@ -4,14 +4,20 @@ selection = "nll"
 
 
 def add_item():
-    to_dos.append(input("Item to add?\n    "))
+    to_dos.append(input("Item to add?\n"))
 
 def check_item():
-    item = input("Item to check off?\n")
-    to_dos[item] = to_dos[item],"[DONE]"
+    try:
+        print("Item to check off?")
+        print_list(to_dos)
+        item = int(input())
+        to_dos[item] = to_dos[item],"[DONE]"
+    except(ValueError):
+        clear()
+        print("Oops, thats not a valid choice. Try again.\n")
 
 def remove_item():
-    to_dos.remove(input("Item to remove?\n    "))
+    to_dos.remove(input("Item to remove?\n"))
 
 def clear():
     # For Windows
@@ -26,12 +32,11 @@ def print_list(lst):
     while ptr < len(lst):
         print(ptr,":",lst[ptr])
         ptr += 1
-    print("\n")
 
 
 while selection != 4:
     try:
-        selection = int(input("1: Add item\n2: Check off item\n3: Remove item\n4: Exit program\n    "))
+        selection = int(input("1: Add item\n2: Check off item\n3: Remove item\n4: Exit program\n"))
         clear()
         if selection == 1:
             add_item()
@@ -45,6 +50,7 @@ while selection != 4:
             raise(ValueError)
         print("Your list is now:")
         print_list(to_dos)
+        print("\n")
     except ValueError:
         clear()
         print("Oops, thats not a valid choice. Try again.\n")
