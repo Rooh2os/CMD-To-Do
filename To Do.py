628e27a533f38fdfd787ca1d8f35530eae3b9369
Rooh2os <reuben.e.h2os@gmail.com>
2025-07-04 10:58:28 -0500

    Fix function calls and improve code structure in To Do list application

--- To Do.py
+++ To Do.py
@@ -1,3 +1,4 @@
+import os
 to_dos = []
 selection = "nll"
 
@@ -5,23 +6,35 @@ selection = "nll"
 def add_item():
     to_dos.append(input("Item to add?\n    "))
     print("To do list is now \n\n",to_dos)
+    exit
 
 def check_item():
     print("err")
+    exit
 
 def remove_item():
     to_dos.remove(input("Item to remove?\n    "))
+    exit
+
+def clear():
+    # For Windows
+    if os.name == 'nt':
+        os.system('cls')
+    # For macOS and Linux
+    else:
+        os.system('clear')
 
 
 while selection != 4:
     try:
         selection = int(input("1: Add item\n2: Check off item\n3: Remove item\n4: Exit program\n    "))
+        clear()
         if selection == 1:
-            add_item
+            add_item()
         elif selection == 2:
-            check_item
+            check_item()
         elif selection == 3:
-            remove_item
+            remove_item()
         else:
             raise(ValueError)
     except ValueError:
