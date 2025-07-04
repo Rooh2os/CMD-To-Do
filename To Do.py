bf283616ea4d5806e8adc578ebe49a3dd9158fdc
Rooh2os <reuben.e.h2os@gmail.com>
2025-07-04 10:51:58 -0500

    Changes to be committed: 	modified:   To Do.py

--- To Do.py
+++ To Do.py
@@ -1,16 +1,28 @@
 to_dos = []
-while selection != 4:
-    try:
-        selection = int(input("1: Add item\n2: Check off item\n3: Remove item\n4: Exit program"))
-        if selection == 1:
-            pass
-    except ValueError:
-        print("Oops, thats not a valid choice. Try again.\n")
-    
-
-
+selection = "nll"
 
 
 def add_item():
-    to_dos.append(input("Item to add?\n     "))
+    to_dos.append(input("Item to add?\n    "))
     print("To do list is now \n\n",to_dos)
+
+def check_item():
+    print("err")
+
+def remove_item():
+    to_dos.remove(input("Item to remove?\n    "))
+
+
+while selection != 4:
+    try:
+        selection = int(input("1: Add item\n2: Check off item\n3: Remove item\n4: Exit program\n    "))
+        if selection == 1:
+            add_item
+        elif selection == 2:
+            check_item
+        elif selection == 3:
+            remove_item
+        else:
+            raise(ValueError)
+    except ValueError:
+        print("Oops, thats not a valid choice. Try again.\n")
