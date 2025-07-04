d452fd0739265801a1074fe9d19f3226bcd4b2d2
Rooh2os <reuben.e.h2os@gmail.com>
2025-07-04 11:20:40 -0500

    Fix: check_item

--- To Do.py
+++ To Do.py
@@ -5,10 +5,10 @@ selection = "nll"
 
 def add_item():
     to_dos.append(input("Item to add?\n    "))
-    print("To do list is now \n\n",to_dos)
 
 def check_item():
-    print("err")
+    item = input("Item to check off?")
+    to_dos[item] = to_dos[item],"[DONE]"
 
 def remove_item():
     to_dos.remove(input("Item to remove?\n    "))
@@ -24,7 +24,7 @@ def clear():
 def print_list(lst):
     ptr = 0
     while ptr < len(lst):
-        print(lst[ptr])
+        print("    ",lst[ptr])
         ptr += 1
 
 
@@ -38,8 +38,11 @@ while selection != 4:
             check_item()
         elif selection == 3:
             remove_item()
+        elif selection == 4:
+            break
         else:
             raise(ValueError)
+        print("Your list is now:\n")
         print_list(to_dos)
     except ValueError:
         print("Oops, thats not a valid choice. Try again.\n")
